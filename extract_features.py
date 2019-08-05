65c87f4ca184c6a75a42f82c1b9529cc616fe825
David Mueller <daveandym@gmail.com>
2019-08-05 12:09:28 -0400

    feature extraction script

--- extract_features.py
+++ extract_features.py
@@ -1,35 +1,75 @@
 import argparse as ap
 import torch
 import torch.nn as nn
+import numpy as np
 
+import data
 import model
+
+def get_args():
+    p = ap.ArgumentParser()
+    p.add_argument('--corpus', type=str, 
+                   default="data/enwik8/corpus.data")
+    p.add_argument('--model', type=str, default='LSTM')
+    p.add_argument('--emsize', type=int, default=400)
+    p.add_argument('--nhid', type=int, default=1840)
+    p.add_argument('--nlayers', type=int, default=3)
+    p.add_argument('--resume', type=str, default=None)
+    p.add_argument('--sentences-file', default=None,
+                    help="Sentences input file")
+    p.add_argument('--vector-outs', default=None,
+                    help="Where to dump sentence representations")
+    p.add_argument('--cuda', action='store_false', help='use CUDA')
+    return p.parse_args()
+
+def load_sentences(sent_file, c):
+    all_sentences = []
+    with open(sent_file, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if line == "":
+                continue
+            symbols = line.split()
+            enc_sent = [c.dictionary.word2idx[sym] for sym in symbols]
+            all_sentences.append(enc_sent)
+    return all_sentences
 
 def model_load(fn):
     global model, criterion, optimizer
     with open(fn, 'rb') as f:
         model, criterion, optimizer = torch.load(f)
 
-model = model.RNNModel(args.model, ntokens, args.emsize, args.nhid, args.nlayers, args.dropout, args.dropouth, args.dropouti, args.dropoute, args.wdrop, args.tied)
+### Main
 
-# load and eval mode to disable dropout etc.
-model_load(args.resume)
+args = get_args()
+fn = args.corpus
+corpus = torch.load(fn)
+ntokens = len(corpus.dictionary)
+
+## Model 
+model = model.RNNModel(args.model, ntokens, args.emsize, 
+                       args.nhid, args.nlayers, tie_weights=True)
+if args.resume is not None:
+    print(f'Loading model from {args.resume}')
+    model_load(args.model)
+if args.cuda:
+    model = model.cuda()
 model.eval()
 
-fn = 'corpus.data'
-fn = os.path.join('', fn)
+## Main Loop
+all_vectors = []
+for sent in load_sentences(args.sentences_file, corpus):
+    data = torch.tensor([sent])
+    data = data.view(1, -1).t().contiguous()
+    if args.cuda:
+        data = data.cuda()
+    hidden = model.init_hidden(1)
+    model_outs = model(data, hidden, return_h=True)
+    result, hidden, raw_outputs, outputs = model_outs
+    sent_outputs = raw_outputs[args.nlayers - 1]
+    sent_outputs = sent_outputs.squeeze().detach().numpy()
+    all_vectors.append(sent_outputs)
 
-corpus = torch.load(fn)
-
-sent = bytes('David is not happy.', encoding='utf-8')
-enc_sent = [corpus.dictionary.word2idx[word] for word in sent]
-
-print(enc_sent)
-
-hidden = model.init_hidden(batch_size)
-model_outs = model(enc_sent, hidden, return_h=True)
-
-result, hidden, raw_outputs, outputs = model_outs
-
-# there should be no difference here
-print(raw_outputs)
-print(outputs)
+print(f"Extracted features for {len(all_vectors)} sentences.")
+print(f"Saving the representations here: {args.vector_outs}")
+np.save(args.vector_outs, all_vectors)
